905879d75bca616e00965e9a5ad3d32929d9184a
lelderbe <fvdc@ya.ru>
2020-11-22 21:52:30 +0300

    ft_putnbr fixed

--- c00/ex07/ft_putnbr.c
+++ c00/ex07/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: lelderbe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/10 21:29:22 by lelderbe          #+#    #+#             */
-/*   Updated: 2020/09/10 21:53:35 by lelderbe         ###   ########.fr       */
+/*   Updated: 2020/09/24 13:04:22 by lelderbe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,24 @@
 void	ft_putnbr(int nb)
 {
 	char c;
+	char sign;
 
-	if (nb >= 10)
+	sign = '-';
+	if (nb >= 10 || nb <= -10)
 	{
 		ft_putnbr(nb / 10);
 	}
-	c = '0' + nb % 10;
+	if (nb < 0)
+	{
+		c = '0' + (-1) * (nb % 10);
+	}
+	else
+	{
+		c = '0' + nb % 10;
+	}
+	if (nb < 0 && nb > -10)
+	{
+		write(1, &sign, 1);
+	}
 	write(1, &c, 1);
 }
